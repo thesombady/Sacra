11e3cf9b5de0b9e4fc0a5fb30957453046184514
Andreas Evensen <andreas.evensen11@gmail.com>
2020-07-11 16:10:23 +0200

    test

--- Sacra/BuildGame/Build.py
+++ Sacra/BuildGame/Build.py
@@ -2,5 +2,7 @@
 from BuildWorld import BuildWorld
 
 class Build:
-    def __init__(self, WorldType):
+    def __init__(self, WorldType, Multiplayer):
+        self.WorldType = WorldType
+        self.Multiplayer = Multiplayer
         pass
--- Sacra/BuildGame/BuildWorld/BuildWorld.py
+++ Sacra/BuildGame/BuildWorld/BuildWorld.py
@@ -3,5 +3,9 @@
 class BuildWorld:
     """ A terrain Builder, refers to Perlin-noise. """
 
-    def __init__(self, Per):
+    def __init__(self, PerlinNoise = True):
+        if PerlinNoise == False:
+            pass
+        else:
+            self.PerlinNoise = PerlinNoise($0)
         pass
