9b1f4ecbe0ccc352164776cc71efa637800eb345
Andreas Evensen <andreas.evensen11@gmail.com>
2020-07-13 20:00:46 +0200

    test

--- Sacra/Renderer/Renderer.py
+++ Sacra/Renderer/Renderer.py
@@ -8,18 +8,56 @@ import json
 import SacraMathEngine as me
 
 
-class Renderer(tk.Frame):
+class Renderer(tk.Canvas):
     """Renderer Class; Lies inside a Frame in tkinter. The renderer will draw the shapes corresponding to each mesh. """
     def __init__(self, master = None):
         super().__init__(master)
         self.master = master
         self.master
         self.master.geometry(f'400x600')
+        self.Masterpath = os.path.join(os.getcwd(), '/Saves')
 
 
     def DrawObject(self, Object):
-        """ DrawObject will draw the object of which one puts as argument. This has to be the full path! """
-        pass
+        """ DrawObject will draw the object of which one puts as argument. This requires the name of of file, not the file-extension itself. """
+        ObjectToDraw = os.path.join(self.Masterpath, Object + '.json')
+        print(type(ObjectToDraw))
+
+
+
+
+Cube = """{
+    "Cube" : [{
+    "1" : "Triangle(vec3d(0, 0, 0), vec3d(0, 1, 0), vec3d(1, 1, 0))"},
+    {
+    "2": "Triangle(vec3d(0, 0, 0), vec3d(1, 1, 0), vec3d(1, 0, 0))"},
+    {
+    "3": "Triangle(vec3d(1, 0, 0), vec3d(1, 1, 0), vec3d(1, 1, 1))"},
+    {
+    "4": "Triangle(vec3d(1, 0, 0), vec3d(1, 1, 1), vec3d(1, 0, 1))"},
+    {
+    "5": "Triangle(vec3d(1, 0, 1), vec3d(1, 1, 1), vec3d(0, 1, 1))"},
+    {
+    "6": "Triangle(vec3d(1, 0, 1), vec3d(0, 1, 1), vec3d(0, 0, 1))"},
+    {
+    "7": "Triangle(vec3d(0, 0, 1), vec3d(0, 1, 1), vec3d(0, 1, 0))"},
+    {
+    "8": "Triangle(vec3d(0, 0, 1), vec3d(0, 1, 0), vec3d(0, 0, 0))"},
+    {
+    "9": "Triangle(vec3d(0, 1, 0), vec3d(0, 1, 1), vec3d(1, 1, 1))"},
+    {
+    "10": "Triangle(vec3d(0, 1, 0), vec3d(1, 1, 1), vec3d(1, 1, 0))"},
+    {
+    "11": "Triangle(vec3d(1, 0, 1), vec3d(0, 0, 1), vec3d(0, 0, 0))"},
+    {
+    "12": "Triangle(vec3d(1, 0, 1), vec3d(0, 0, 0), vec3d(1, 0, 0))"}]
+}
+"""
+
+
+test = json.loads(Cube)
+test = json.dumps(test, indent = 4)
+print(test)
 
 
 #root = tk.Tk()
