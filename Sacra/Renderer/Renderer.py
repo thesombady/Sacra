84cfdabee27baf345b3a03ce672b968d093e5ba9
Andreas Evensen <andreas.evensen11@gmail.com>
2020-07-13 21:04:21 +0200

    test

--- Sacra/Renderer/Renderer.py
+++ Sacra/Renderer/Renderer.py
@@ -6,6 +6,7 @@ from tkinter import filedialog
 import json
 #Import Materials #Don't work when importing renderer!
 import SacraMathEngine as me
+from PIL import Image
 
 
 class Renderer(tk.Canvas):
@@ -18,12 +19,14 @@ class Renderer(tk.Canvas):
         self.Masterpath = os.path.join(os.getcwd(), '/Saves')
 
 
-    def DrawObject(self, Object):
+    def DrawObject(self, Object, Name):
         """ DrawObject will draw the object of which one puts as argument. This requires the name of of file, not the file-extension itself. """
         #ObjectToDraw = os.path.join(self.Masterpath, Object + '.json')
         if not isinstance(Object, me.MeshObject):
             raise KeyError("Not correct format")
-        print(type(Object))
+        RealObject = Object
+
+
 
 
 
@@ -63,9 +66,10 @@ test = json.loads(Cube)
 #test = json.dumps(test, indent = 4)
 print(type(test))
 mesh = me.MeshObject(test, 'Cube')
+print(type(mesh))
 
 
 root = tk.Tk()
 app = Renderer(root)
-app.DrawObject(mesh)
+app.DrawObject(mesh, 'Cubes')
 app.mainloop()
